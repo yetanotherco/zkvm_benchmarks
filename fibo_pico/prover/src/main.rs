10deb9ca94907a3ee8503f7e27df6df1ea7c94a1
MauroFab <maurotoscano2@gmail.com>
2025-02-14 14:35:57 -0300

    Simplify pico benches

--- fibo_pico/prover/src/main.rs
+++ fibo_pico/prover/src/main.rs
@@ -1,4 +1,4 @@
-use fibonacci_lib::{FibonacciData, fibonacci, load_elf};
+use fibonacci_lib::{FibonacciData, load_elf};
 use pico_sdk::{client::DefaultProverClient, init_logger};
 
 fn main() {
@@ -20,27 +20,9 @@ fn main() {
     let proof = client.prove_fast().expect("Failed to generate proof");
 
     // Decodes public values from the proof's public value stream.
-    let public_buffer = proof.pv_stream.unwrap();
+    // let public_buffer = proof.pv_stream.unwrap();
 
     // Deserialize public_buffer into FibonacciData
-    let public_values: FibonacciData =
-        bincode::deserialize(&public_buffer).expect("Failed to deserialize");
-
-    // Verify the public values
-    verify_public_values(n, &public_values);
-}
-
-/// Verifies that the computed Fibonacci values match the public values.
-fn verify_public_values(n: u32, public_values: &FibonacciData) {
-    println!(
-        "Public value n: {:?}, a: {:?}, b: {:?}",
-        public_values.n, public_values.a, public_values.b
-    );
-
-    // Compute Fibonacci values locally
-    let (result_a, result_b) = fibonacci(0, 1, n);
-
-    // Assert that the computed values match the public values
-    assert_eq!(result_a, public_values.a, "Mismatch in value 'a'");
-    assert_eq!(result_b, public_values.b, "Mismatch in value 'b'");
+    // let _public_values: FibonacciData =
+    //    bincode::deserialize(&public_buffer).expect("Failed to deserialize");
 }
