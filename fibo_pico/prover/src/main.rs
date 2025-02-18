fd48cf38aa458d01047347c10545c1b22d3769b0
MauroFab <maurotoscano2@gmail.com>
2025-02-17 21:23:44 -0300

    Add run pico without cli

--- fibo_pico/prover/src/main.rs
+++ fibo_pico/prover/src/main.rs
@@ -1,9 +1,26 @@
+use std::fs;
+
+extern crate pico_sdk;
+
+
 use pico_sdk::{client::DefaultProverClient, init_logger};
-use fibonacci_lib::load_elf;
+/// Loads an ELF file from the specified path.
+
+pub fn load_elf(path: &str) -> Vec<u8> {
+    fs::read(path).unwrap_or_else(|err| {
+        panic!("Failed to load ELF file from {}: {}", path, err);
+    })
+}
+
 
 fn main() {
     // Initialize logger
     init_logger();
+
+    let args: Vec<String> = std::env::args().collect();
+    let n = args.get(1)
+        .and_then(|s| s.parse::<u32>().ok())
+        .expect("Missing the number of times to do fibonacci as an argument");
 
     // Load the ELF file
     let elf = load_elf("../elf/riscv32im-pico-zkvm-elf");
@@ -13,11 +30,10 @@ fn main() {
     let stdin_builder = client.get_stdin_builder();
 
     // Set up input
-    let n = 100u32;
     stdin_builder.borrow_mut().write(&n);
 
     // Generate proof
-    let _proof = client.prove_fast().expect("Failed to generate proof");
+    let _proof = client.prove("asd.proof".into()).expect("Failed to generate proof");
 
     // Decodes public values from the proof's public value stream.
     // let public_buffer = proof.pv_stream.unwrap();
