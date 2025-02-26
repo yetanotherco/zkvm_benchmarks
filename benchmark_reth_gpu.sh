aaabda4b735be6899b9a18adf05d9611937e87d2
MauroFab <maurotoscano2@gmail.com>
2025-02-26 00:03:46 -0300

    Fix cuda

--- benchmark_reth_gpu.sh
+++ benchmark_reth_gpu.sh
@@ -30,7 +30,7 @@ OUTPUT_FILE="benchmark_reth_gpu_results.csv"
 
 # Build all projects
 echo "Building all projects..."
-make build_rsp_sp1 RUSTFLAGS="cuda"
+make build_rsp_sp1 SP1_PROVER="cuda"
 make build_rsp_pico
 
 # Initialize results file
