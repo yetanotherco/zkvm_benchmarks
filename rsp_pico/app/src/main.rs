0f5c001131e1c186674a0aa9bef4dcd763a057cf
JuArce <52429267+JuArce@users.noreply.github.com>
2025-03-05 11:47:50 -0300

    update pico rsp example

--- rsp_pico/app/src/main.rs
+++ rsp_pico/app/src/main.rs
@@ -3,21 +3,22 @@
 pico_sdk::entrypoint!(main);
 use pico_sdk::io::{commit, read_vec};
 
-use rsp_client_executor::{io::ClientExecutorInput, ClientExecutor, EthereumVariant};
+use rsp_client_executor::{executor::EthClientExecutor, io::EthClientExecutorInput};
+use std::sync::Arc;
 
 pub fn main() {
     // Read the input.
     let input = read_vec();
-    
-    
-    let input = bincode::deserialize::<ClientExecutorInput>(&input).unwrap();
+    let input = bincode::deserialize::<EthClientExecutorInput>(&input).unwrap();
 
     // Execute the block.
-    let executor = ClientExecutor;
-    let header = executor.execute::<EthereumVariant>(input).expect("failed to execute client");
+    let executor = EthClientExecutor::eth(
+        Arc::new((&input.genesis).try_into().unwrap()),
+        input.custom_beneficiary,
+    );
+    let header = executor.execute(input).expect("failed to execute client");
     let block_hash = header.hash_slow();
 
     // Commit the block hash.
-
     commit(&block_hash);
 }
